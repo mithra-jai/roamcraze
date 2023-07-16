bffa954bfb2658a57dcc3125766894393acde73c
Sangamithra <sangamithrajai@gmail.com>
2023-07-16 13:48:56 +0530

    destination-align

--- components/Countries.js
+++ components/Countries.js
@@ -12,13 +12,13 @@ export default function Countries() {
     { name: "iceland", title: "Iceland", image: "/iceland.jpg" },
   ];
   return (
-    <div className="container mx-auto py-20 px-8 country-main-div">
+    <div className="container mx-auto py-20 px-8">
      <h1 className="text-center mb-10 text-4xl ">Destinations</h1>
     <div className="grid lg:grid-cols-3 gap-6">
-      {countries.map(({ name, title, image,slug }) => (
-        <div className="shadow-lg rounded-lg dest-cards" key={name}>
+      {countries.map(({ name, title, image }) => (
+        <div className="shadow-lg rounded-lg" key={name}>
           <Link href={`/${name}`}>
-            <Image className="rounded-t-lg" src={image} height={300} width={400} alt={slug} />
+            <Image className="rounded-t-lg" src={image} height={500} width={800} alt="thumb" />
           </Link>
           <div className="p-5">
             <h2 className="text-3xl font-bold mb-3">{title}</h2>
